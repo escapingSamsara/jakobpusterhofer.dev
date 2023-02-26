178b5c5356f892c7580cede129e4a0752dd91336
escapingSamsara <jpusterhofer@gmail.com>
2023-02-26 20:43:39 +0100

    setup touch handlers for cta=buttons

--- main.js
+++ main.js
@@ -13,139 +13,43 @@ window.addEventListener('load', function () {
 })
 //preloading screen end
 
-//wiggle button effet:
-// const card = document.querySelector('.cta-btn-resume')
-// window.addEventListener('mousemove', (e) => {
-//   let x = e.pageX - innerWidth / 2
-//   let y = e.pageY - innerHeight / 2
-//   card.style.transform = `translate(${x / 40}px, ${y / 40}px)`
-// })
-
-// //text morph
-// /*
-// 	This pen cleverly utilizes SVG filters to create a "Morphing Text" effect. Essentially, it layers 2 text elements on top of each other, and blurs them depending on which text element should be more visible. Once the blurring is applied, both texts are fed through a threshold filter together, which produces the "gooey" effect. Check the CSS - Comment the #container rule's filter out to see how the blurring works!
-// */
-
-// const elts = {
-//   text1: document.getElementById('text1'),
-//   text2: document.getElementById('text2'),
-// }
-
-// // The strings to morph between. You can change these to anything you want!
-// const texts = ['Click', 'to', 'find', 'out', 'more', 'about', 'me']
-
-// // Controls the speed of morphing.
-// const morphTime = 1
-// const cooldownTime = 0.25
-
-// let textIndex = texts.length - 1
-// let time = new Date()
-// let morph = 0
-// let cooldown = cooldownTime
-
-// elts.text1.textContent = texts[textIndex % texts.length]
-// elts.text2.textContent = texts[(textIndex + 1) % texts.length]
-
-// function doMorph() {
-//   morph -= cooldown
-//   cooldown = 0
-
-//   let fraction = morph / morphTime
-
-//   if (fraction > 1) {
-//     cooldown = cooldownTime
-//     fraction = 1
-//   }
-
-//   setMorph(fraction)
-// }
-
-// A lot of the magic happens here, this is what applies the blur filter to the text.
-// function setMorph(fraction) {
-//   // fraction = Math.cos(fraction * Math.PI) / -2 + .5;
-
-//   elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`
-//   elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`
-
-//   fraction = 1 - fraction
-//   elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`
-//   elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`
-
-//   elts.text1.textContent = texts[textIndex % texts.length]
-//   elts.text2.textContent = texts[(textIndex + 1) % texts.length]
-// }
-
-// function doCooldown() {
-//   morph = 0
-
-//   elts.text2.style.filter = ''
-//   elts.text2.style.opacity = '100%'
-
-//   elts.text1.style.filter = ''
-//   elts.text1.style.opacity = '0%'
-// }
-
-// // Animation loop, which is called every frame.
-// function animate() {
-//   requestAnimationFrame(animate)
-
-//   let newTime = new Date()
-//   let shouldIncrementIndex = cooldown > 0
-//   let dt = (newTime - time) / 1000
-//   time = newTime
-
-//   cooldown -= dt
-
-//   if (cooldown <= 0) {
-//     if (shouldIncrementIndex) {
-//       textIndex++
-//     }
-
-//     doMorph()
-//   } else {
-//     doCooldown()
-//   }
-// }
-
-// // Start the animation.
-// animate()
-
 ///////////////////////////////////////////////
-//TOUCH EVENTS
-///////////////////////////////////////////////
-
-// touch events
-document.addEventListener('touchstart', function (e) {
-  console.log('touchstart')
-
-  e.preventDefault()
-  e.stopImmediatePropagation()
-  e.stopPropagation()
-})
-document.addEventListener('touchend', function (e) {
-  console.log('touchend')
-
-  e.preventDefault()
-  e.stopImmediatePropagation()
-  e.stopPropagation()
-})
-//------------------------------------------------------------
-
-///////////////////////////////////////////////
-//GSAP CURSOR
+//GSAP CURSOR AND TOUCH EVENTS
 ///////////////////////////////////////////////
 window.addEventListener('load', function () {
-  //GSAP CURSOR Start
+  ///////////////////////////////////////////////
+  //GSAP CURSOR
   gsap.set('.gsapCursor', { xPercent: -50, yPercent: -50 })
-
   let gsapCursor = document.querySelector('.gsapCursor')
-
   window.addEventListener('mousemove', (e) => {
     gsap.to(gsapCursor, 0.2, { x: e.clientX, y: e.clientY })
   })
   document.addEventListener('mousedown', function () {})
   document.addEventListener('mouseup', function () {})
 
+  ///////////////////////////////////////////////
+  //TOUCH EVENTS
+
+  //cta-btn Touch Event:
+  let ctaBtn = document.querySelectorAll('.cta-btn')
+  ctaBtn.addEventListener('touchstart', () => {
+    ctaBtn.classList.add('cta-btnTouch')
+  })
+  //AboutMe Touch Events:
+  let chart = document.querySelectorAll('.chart')
+  chart.addEventListener('touchstart', () => {
+    chart.classList.add('chartTouch')
+  })
+
+  let portfolioContentProjectImageMacbook = document.querySelector(
+    '.portfolioContentProjectImageMacbook'
+  )
+  portfolioContentProjectImageMacbook.addEventListener('touchstart', () => {
+    chart.classList.add('portfolioContentProjectImageMacbookTouch')
+  })
+  portfolioContentProjectImageMacbook.addEventListener('touchend', () => {
+    chart.classList.remove('portfolioContentProjectImageMacbookTouch')
+  })
   // function addHoverHandlers(elements, className) {
   //   elements.forEach((element) => {
   //     element.addEventListener('mouseover', () => {
